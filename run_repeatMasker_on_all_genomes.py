7e21c3d6aee4f77d309786ad7d054eb442c5d18d
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-10-20 12:37:33 -0700

    Added python script to run RepeatMasker on the 248 genomes.

--- run_repeatMasker_on_all_genomes.py
+++ run_repeatMasker_on_all_genomes.py
@@ -18,11 +18,15 @@ def run_repeatMasker(genome_path):
     output_log_path = os.path.join(OUT_DIR, str(GENOMES_DIR.split('/')[-1] + ".out"))
     repeat_masker_command = ".{} --species mammals {}".format(REPEAT_MASKER_EXC, genome_path)
 
+    print(output_log_path)
+    print(repeat_masker_command)
+
     start_time = time.time()
     #repeat_masker_process = subprocess.Popen(repeat_masker_command, shell=True, stdout=output_log_path, stderr=subprocess.PIPE)
     #rm_stderr = repeat_masker_process.communicate()[1]
-    repeat_masker_process = subprocess.run([str('.' + REPEAT_MASKER_EXC), '--species', 'mammals', genome_path], stdout=output_log_path, universal_newlines=True)
-    print("RepeatMasker completed on {} genome.\n".format(genome_path))
+    # repeat_masker_process = subprocess.run([str('.' + REPEAT_MASKER_EXC), '--species', 'mammals', genome_path], stdout=output_log_path, universal_newlines=True)
+    
+    print("RepeatMasker completed on {} genome in {} seconds.\n".format(genome_path, str(time.time() - start_time)))
 
 def main():
 
@@ -32,11 +36,10 @@ def main():
         print(genome)
     #print(genome_path_list)
 
-    #job_pool = multiprocessing.Pool()
-    #for repeat_masker_result in job_pool.imap_unordered(run_repeatMasker, )
+    job_pool = multiprocessing.Pool()
+    for repeat_masker_result in job_pool.imap_unordered(run_repeatMasker, genome_path_list):
+        print(repeat_masker_result)
 
-
-    
 
 if __name__=='__main__':
     main()
