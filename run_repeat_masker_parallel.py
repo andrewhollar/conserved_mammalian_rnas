2eeb33a2c84ac895906c5cf29839e9986aafcfc3
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-10-22 15:11:46 -0700

    changed RepeatMasker parameters

--- run_repeat_masker_parallel.py
+++ run_repeat_masker_parallel.py
@@ -11,7 +11,7 @@ REPEAT_MASKER_EXC = '/home/ahollar/software/RepeatMasker/RepeatMasker'
 
 def run_repeatMasker(genome_path):
     output_log_path = open(os.path.join(OUT_DIR, str(str(genome_path).split('/')[-1] + ".out")), 'w')
-    repeat_masker_process = subprocess.run([REPEAT_MASKER_EXC, '--species', 'mammals', '--par', '30', '--q', str(genome_path)], stdout=output_log_path, universal_newlines=True)    
+    repeat_masker_process = subprocess.run([REPEAT_MASKER_EXC, '--species', 'mammals', '--par', '45', '--qq', str(genome_path)], stdout=output_log_path, universal_newlines=True)    
     output_log_path.close()
     return repeat_masker_process
 
@@ -19,10 +19,13 @@ def main():
     if not os.path.isdir(OUT_DIR):
         os.makedirs(OUT_DIR)
 
-    genome_path_list = Path(GENOMES_DIR).glob('**/*.fna')
+    genome_path_list = Path(GENOMES_DIR).glob('**/*.fna ')
 
     for genome in genome_path_list:
         start_time = time.time()
+        if os.path.exists("".join(str(genome), ".masked")):
+            print("RepeatMasker already completed on the {} genome.".format(str(genome)))
+            continue
         repeat_masker_output = run_repeatMasker(str(genome))
         print("RepeatMasker completed on {} genome in {} seconds.\n".format(str(genome), str(time.time() - start_time)))
 
